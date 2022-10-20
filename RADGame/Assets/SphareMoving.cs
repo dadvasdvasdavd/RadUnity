7e04b7427c7c4ea57dc6bdaccbd5908023e3919c
STUDENT David Vajdik <David.Vajdik@students.ittralee.ie>
2022-10-20 11:02:32 +0100

    Signed-off-by: STUDENT David Vajdik <David.Vajdik@students.ittralee.ie>

--- RADGame/Assets/SphareMoving.cs
+++ RADGame/Assets/SphareMoving.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SphareMoving : MonoBehaviour
 {
+    float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,30 @@ public class SphareMoving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.up;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += speed * transform.forward * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += speed * Vector3.down * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += speed * Vector3.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += speed * Vector3.left * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, 5f);
+        }
+
     }
 }
